91dbfde0b9e91d769165458cd6701bb265be7c96
Lizzie Anthony <lizziewrab@me.com>
2021-12-03 12:54:10 -0600

    commit tree 7585f3da70c14fa08353681bd5bab173a551c02b

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize (str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  static sanitize (str) {
+    return str.replace(/[^A-Za-z0-9-' ']/g, '')
+  }
+
+    static titleize (str) {
+      const words = str.toLowerCase().split(" ");
+    let newWords = words.map(word => {
+      if ( word !== 'the' && word !== 'a' && word !== 'an' && word !== 'but'  && word !== 'of' && word !== 'and' && word !== 'for' && word !== 'at' && word !== 'by' && word !== 'from'){
+        return word[0].toUpperCase() + word.slice(1)
+      } else {
+        return word
+      }
+    }).join(" ")
+    return newWords[0].toUpperCase() + newWords.slice(1)
+    }
 }
